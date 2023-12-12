2452bd43f6997b5d725d2e1f82917fbf9714c2dd
krish1770 <rameshkrishnaprasath@gmail.com>
2023-12-12 11:53:22 +0530

    initial commit1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React,{useState} from 'react';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Seating from './Components/Seating';
+
 function App() {
+
+  const [details,setDetails]=useState(
+    {
+      // companyName:"Divum",
+    });
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <BrowserRouter>
+    <Routes>
+      <Route
+      path='/'
+      element={
+        <Seating
+          // details={details}
+          // setDetails={setDetails}
+        ></Seating>
+      }
+      >
+
+      </Route>
+
+
+    </Routes>
+    
+    </BrowserRouter>
+  
   );
 }
 
